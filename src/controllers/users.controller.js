b3afd2b7ebdb7b2a7d7378c297317d091499eb5d
WalterJoel <walter.valdivia@ucsp.edu.pe>
2024-10-04 11:41:48 -0500

    [feat] SET DB

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,9 @@
+import pool from '../db.js';
+
 export const getAllUsers = async (req, res) => {
   //MOCK DB
   const users = ['walter', 'gabriel', 'KAREN'];
+  const posts = await pool.query('SELECT email FROM user;');
 
-  return res.send(users);
+  return res.json({ posts });
 };
--- src/db.js
+++ src/db.js
@@ -1,8 +1,11 @@
-import { Pool } from 'pg';
+import pg from 'pg';
+
 import { RAILWAY_CONNECTION_URL } from './config.js';
 
 // The secret connection string you copied earlier
 const connectionString = RAILWAY_CONNECTION_URL;
+const { Pool } = pg;
+
 const pool = new Pool({
   connectionString,
 });
