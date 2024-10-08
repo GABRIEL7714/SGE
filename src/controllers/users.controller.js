fb8a3924887a7ce9ecff003a87280f41de4ba1e7
WalterJoel <walter.valdivia@ucsp.edu.pe>
2024-10-07 21:40:44 -0500

    [feat] Create User

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,3 +6,20 @@ export const getAllUsers = async (req, res) => {
 
   return res.json(posts);
 };
+
+export const createUser = async (req, res) => {
+  let { nombre, apellido, correo, telefono, contrasena, tipodoc, numerodoc } =
+    req.body;
+
+  try {
+    const user = await pool.query(
+      'INSERT INTO usuario (nombre, apellido, correo, telefono, contraseña, tipodoc, numerodoc) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [nombre, apellido, correo, telefono, contrasena, tipodoc, numerodoc]
+    );
+
+    return res.json(user.rows[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Error creating user' });
+  }
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 //Obtengo las funciones del controlador
-import { getAllUsers } from '../controllers/users.controller.js';
+import { getAllUsers, createUser } from '../controllers/users.controller.js';
 
 const router = Router();
 
 //Defino nombres de las rutas,
 router.get('/getAllUsers', getAllUsers);
+router.post('/createUser', createUser);
 
 export default router;
